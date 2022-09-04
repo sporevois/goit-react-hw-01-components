a72c546135508f7715bf1852d6d868d531fbdd73
Stanislav Sporevoi <stanislav.sporevoi@gmail.com>
2022-09-05 00:20:18 +0300

    makes render

---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+
+const FriendList = ({friends}) => {
+    return (
+        <ul class="friend-list">
+            {friends.map(friend => {
+                const { id, avatar, name, isOnline } = friend;
+                return (
+                    <li key={id} class="item">
+                        <span class="status"></span>
+                        <img class="avatar" src={avatar} alt={name + " " + "avatar"} width="48" />
+                        <p class="name">{name}</p>
+                    </li>
+                )
+            })};
+        </ul>
+    )
+}
+
+FriendList.propTypes = {
+    friends: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            isOnline: PropTypes.bool.isRequired,
+            name: PropTypes.string.isRequired,
+            avatar: PropTypes.string.isRequired,
+        }),
+    ),
+};
+
+export default FriendList
+
+    
